bd7b9e145be7b4f86d6375067c31a53ec54fae3a
devgiants75 <devgiants75@naver.com>
2024-07-29 21:35:04 +0900

    20240729 structure

---
+++ src/main/java/com/project/jjj_was/JjjWasApplication.java
@@ -0,0 +1,13 @@
+package com.project.jjj_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjjWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjjWasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jjj_was/JjjWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jjj_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjjWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
